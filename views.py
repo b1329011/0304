39f94ecec9af3bbc2288c92d09f535060f446196
b1329011 <lisihyu@gmail.com>
2026-03-11 16:04:28 +0800

    Update API endpoint to use Django REST framework
    Refactor hello_api to myIndex and add logging.

--- views.py
+++ views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import logging
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-def hello_api(request):
-    my_name = request.GET.get('name',"CGU")
+logger = logging.getLogger('django')
 
-    return HttpResponse(f"Hello " + {my_name})
+@api_view(['GET'])
+def myIndex(request):
+    my_name = request.GET.get('name', None)
+    
+    logger.debug("************** myhello_api: " + str(my_name))
+    
+    if my_name:
+        return Response({"data": "Hello " + my_name}, status=status.HTTP_200_OK)
+    else:
+        return Response(
+            {"res": "parameter: name is None"}, 
+            status=status.HTTP_400_BAD_REQUEST
+        )
