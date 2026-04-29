40e2c6c60922cde62388aceabd0685c4d70274f1
b1329011 <lisihyu@gmail.com>
2026-04-29 14:40:20 +0800

    Delete views.py

--- views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def hello_api(request):
-    my_name = request.GET.get('name',"CGU")
-
-    return HttpResponse(f"Hello " + {my_name})
